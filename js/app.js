a95ff628dfe341186009fb193e02ea95c8912247
AnsarMaxutov <ansar.maxutov@gmail.com>
2024-02-18 18:50:10 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,116 @@
+// let firstName = 'John'
+// let lastName = 'Doe'
+// let age = 30
+// let work = "IT"
+
+
+// let info = 'Его зовут ' + firstName + '. Его фамилия ' +  lastName + '. Ему ' + age + ' лет.' + "Работает в сфере " + work
+
+
+// let info = `Его зовут ${firstName}. Его фамилия ${lastName}. Ему ${age} лет. Работает в сфере ${work}`
+
+// console.log(info)
+
+// -----------------------------------------------
+
+// let data = ['John', 'Hello', 76, [100, 250, [40, 50, 60], 540], 88, 'just']
+// console.log(data[3][2][2]) 
+
+// -----------------------------------------
+
+// let usersTraficOne = ['John', 'Bob', 'Sam', 'Adam']
+// let usersTraficTwo = ['Robert', 'Tom', 'Josh']
+// let usersTraficThree = ['Jarry', 'Case', 'Sonya', 'Jax']
+
+// let allUsers = [
+//     usersTraficOne,
+//     usersTraficTwo, 
+//     usersTraficThre
+// ]
+
+// console.log(allUsers)
+
+// ------------------------------------------
+// undefined
+
+// undefined
+// let a
+// console.log(a)
+
+// -----------------------------
+
+// null
+
+// let a = 89
+// a = null 
+// console.log(a)
+
+// -----------------------------
+
+// Boolean (Логический тип данных)
+
+// console.log(true)
+// console.log(false)
+
+
+// let a = confirm('Pokinut sait?')
+// console.log(a)
+
+
+// ---------------------------
+// Условие (if)
+
+// if(10 > 4){
+//     console.log('Hello')
+// }
+
+// --------
+// let updateBrowser = confirm('Обновить Google Chrome?')
+
+// if(updateBrowser == true){
+//     alert('Ваш браузер был обновлен!')
+// }
+
+// -------
+
+// let firstName = prompt('Whats ur name?')
+
+// if(firstName == 'Tom'){
+//     alert('Hello admin')
+// }
+
+// -------
+
+// else ????
+
+
+let player1 = 'Hazard'
+let player2 = 'Salah'
+
+let positionPlayer1 = 'left Winger'
+
+let positionPlayer2 = 'right Winger'
+
+let team = player1 + 'plays ' + 'by '+ positionPlayer1 + '. What about' + player2 + '. He is a  ' + positionPlayer2 + '. '
+
+console.log(team)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
